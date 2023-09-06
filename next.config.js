242c61cf5946903af657f146c4c4b015a76e943d
Hyacinthe ABANDA <hyacinthe@izytechgroup.com>
2023-09-06 20:56:47 +0200

    fix error on load external image

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@ const nextConfig = {
   },
   reactStrictMode: true,
   transpilePackages: ["@slate-yjs/react"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'liveblocks.io',
+        port: '',
+        pathname: '/avatars/**',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
